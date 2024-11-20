d9a3fd18aeac340aa8cb96d83d4f8746c5e5bdf0
Hardeep Singh <hardeep.space@gmail.com>
2024-11-20 13:51:55 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Location from './components/network/Location.jsx';
 import Entity from './components/entity/Entity.jsx';
 import FourZeroFour from './components/fourzerofour/FourZeroFour.jsx';
 import Work from './components/work/Work.jsx';
+import Services from './components/services/Services.jsx';
+import Header from './components/layout/Header.jsx';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
         <Route path='/location' element={<Location />} />
         <Route path='/entity' element={<Entity />} />
         <Route path='/work' element={<Work />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/header' element={<Header />} />
         <Route path='*' element={<FourZeroFour />} />
       </Routes>
     </Router>
