44e2a14ae4e03481334ac65e5219c950166fc5c1
Reetik Pant <reetik.mslindia@gmail.com>
2024-11-20 13:46:15 +0530

    reetik appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ import Network from './components/network/Network.jsx';
 import Location from './components/network/Location.jsx';
 import Entity from './components/entity/Entity.jsx';
 import FourZeroFour from './components/fourzerofour/FourZeroFour.jsx';
-import Work from './components/work/Work.jsx';
+import Work from './pages/Work';
+import CaseStudy from './pages/Case';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/network' element={<Network />} />
@@ -17,8 +21,12 @@ function App() {
         <Route path='/entity' element={<Entity />} />
         <Route path='/work' element={<Work />} />
         <Route path='*' element={<FourZeroFour />} />
+        <Route path="/work/case-study/:slug" element={<CaseStudy />} />
+
       </Routes>
+      <Footer />
     </Router>
+    
   );
 }
 
