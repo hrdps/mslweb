52cf67a55da77f0459f974fdb9dd348ad15f4c8f
Hardeep Singh <105967614+hrdps@users.noreply.github.com>
2024-12-03 16:07:28 +0530

    Update Loading.jsx

--- src/components/layout/Loading.jsx
+++ src/components/layout/Loading.jsx
@@ -8,7 +8,7 @@ const Loading = () => {
       alignContent={'center'}
       minH={'100vh'}
       zIndex={999}>
-      <Img src='/assets/mslLoader.webp' w={'18%'}></Img>
+      <Img src='https://res.cloudinary.com/depykl2hm/image/upload/v1733222092/mslLoader_jghdaj.webp' w={'15%'}></Img>
     </VStack>
   );
 };
